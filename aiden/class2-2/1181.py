515b7f07d50a35c6e9123b3a194f13755bcb2abd
HanMulBit98 <dubuji12@gmail.com>
2024-08-03 20:28:59 +0900

    #19 1181.py

---
+++ aiden/class2-2/1181.py
@@ -0,0 +1,17 @@
+import sys
+
+
+input = sys.stdin.read
+data = input().strip().split()
+
+
+n = int(data[0])
+words = data[1:]
+
+
+words = list(set(words))
+words.sort(key=lambda x: (len(x), x))
+
+
+for word in words:
+    print(word)
