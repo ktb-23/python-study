b5a6e58fa9d0ffd4cb0d9eb5c45a0d92b6866500
HanMulBit98 <dubuji12@gmail.com>
2024-08-03 20:30:06 +0900

    #19 10814.py

---
+++ aiden/class2-2/10814.py
@@ -0,0 +1,16 @@
+n = int(input())
+member = []
+
+
+for i in range(n):
+    age, name = input().split() 
+    age = int(age)
+    member.append((age, name))
+    
+    
+member.sort(key=lambda x: x[0])
+
+
+for age, name in member:
+    print(age,name)
+    
