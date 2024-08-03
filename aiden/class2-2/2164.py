373eccdb003821f050dc34ae872711726f7a646d
HanMulBit98 <dubuji12@gmail.com>
2024-08-03 20:31:23 +0900

    #19 2164.py

---
+++ aiden/class2-2/2164.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+
+input = sys.stdin.read
+
+n = int(input().strip())  
+cards = deque(range(1, n + 1))
+
+
+while len(cards) > 1:
+    cards.popleft()  
+    cards.rotate(-1)  
+
+
+print(cards.pop())
