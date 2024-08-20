6f838dc5d3184df1107189bdf2d9b7f2718cf98f
aiden.ji(지윤수) / 인공지능 <dubuji12@gmail.com>
2024-08-21 08:55:26 +0900

    #27 1012.py 수정

--- aiden/class3-1/1012.py
+++ aiden/class3-1/1012.py
@@ -1,16 +1,41 @@
-def move(n,r,c):
-    if n == 0:
-        return 0
-    half = 2**(n-1)
-    # 각 사분면
-    if r < half and c < half:
-        return move(n-1, r, c)
-    elif r < half and c >= half:
-        return half * half + move(n-1, r, c-half)
-    elif r >= half and c < half:
-        return 2 * half * half + move(n-1, r-half, c)
-    else:
-        return 3 * half * half + move(n-1, r-half, c-half)
+from collections import deque
 
-n, r, c = map(int,input().split())
-print(move(n,r,c))
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    visited[x][y] = True
+    
+    while queue:
+        x, y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and field[nx][ny] == 1:
+                queue.append((nx, ny))
+                visited[nx][ny] = True
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+t = int(input()) 
+
+for _ in range(t):
+    m, n, k = map(int, input().split())  
+    field = [[0] * m for _ in range(n)]  
+    visited = [[False] * m for _ in range(n)]  
+    
+    for _ in range(k):
+        x, y = map(int, input().split())
+        field[y][x] = 1
+    
+    count = 0  
+    
+    for i in range(n):
+        for j in range(m):
+            if field[i][j] == 1 and not visited[i][j]:
+                bfs(i, j)
+                count += 1
+    
+    print(count)
