f2414fb22801b040683744b3fa72b43415c2b807
kwangyoulsagong <sgky0511@naver.com>
2024-08-25 22:05:31 +0900

    #32 11654.js

---
+++ hardy/class1-1/11654.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+const input = fs.readFileSync(file).toString().trim().split("\n");
+const a = input[0];
+console.log(a.charCodeAt(0));
