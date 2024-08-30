cd82a060f25552e4f088397506223688baf3cbff
MoiJiyeon <118200927+MoiJiyeon@users.noreply.github.com>
2024-08-30 22:38:16 +0900

    #38 1629.py 파일 수정

--- luna/class4-1/1629.py
+++ luna/class4-1/1629.py
@@ -1,6 +1,2 @@
-A, B, C = map(int,open(0).read().split())
-
-result = 1  # A**0 = 항상 1
-for _ in range(B):
-    result = result * A % C
-print(result)
+A, B, C = map(int, input().split())
+print(pow(A, B, C))
